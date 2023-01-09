83b1a821716b00eea0dcf223d4274682b6c50a67
kota <52403688+yashikota@users.noreply.github.com>
2023-01-09 19:18:39 +0900

    update delete method

--- regulate.py
+++ regulate.py
@@ -4,32 +4,21 @@ import glob
 
 # 引数を解析
 parser = argparse.ArgumentParser()
-parser.add_argument("target_dir")
+parser.add_argument("input_dir")
+parser.add_argument("output_dir")
 args = parser.parse_args()
 
-# 処理対象のディレクトリを取得
-os.chdir(args.target_dir)
+# input_dirのファイル一覧を取得
+input_files = [os.path.basename(file) for file in glob.glob(os.path.join(args.input_dir, "*"))]
 
-# ファイルのリストを取得
-files = list(os.path.splitext(os.path.basename(file))[0] for file in glob.glob("*"))
+# output_dirの.txtファイルでないファイル一覧を取得
+output_files = [os.path.basename(file) for file in glob.glob(os.path.join(args.output_dir, "*")) if os.path.splitext(file)[1] != ".txt"]
 
-# 名前が重複していないファイルを削除
-for file in files:
-    if files.count(file) < 2:
-        try:
-            os.remove(file + ".jpg")
-            print(f"削除: {file}.jpg")
-        except:
-            pass
-        try:
-            os.remove(file + ".png")
-            print(f"削除: {file}.png")
-        except:
-            pass
-        try:
-            os.remove(file + ".txt")
-            print(f"削除: {file}.txt")
-        except:
-            pass
+diff_files = list(set(input_files) - set(output_files))
+
+if len(diff_files) != 0:
+    for file in diff_files:
+        print(f"削除: {file}")
+        os.remove(os.path.join(args.input_dir, file))
 
 print("終了")
